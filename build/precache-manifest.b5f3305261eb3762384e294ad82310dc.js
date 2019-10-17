548f785e3dca58cf543355e5d5bb87af3dacac90
Joshua Pozos <joshua@pozoss.com>
2019-10-17 12:48:28 -0500

    Add: [monsters-rolodex] Current Version

---
+++ build/precache-manifest.b5f3305261eb3762384e294ad82310dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3621359a27307fde2c1d71341bfcb7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b498bdd5874a78e9c344",
+    "url": "/monster-rolodex/static/css/main.d6b14491.chunk.css"
+  },
+  {
+    "revision": "e89ff0de3480fcf68967",
+    "url": "/monster-rolodex/static/js/2.9573cf0d.chunk.js"
+  },
+  {
+    "revision": "b498bdd5874a78e9c344",
+    "url": "/monster-rolodex/static/js/main.dee89c2c.chunk.js"
+  },
+  {
+    "revision": "c077d3a720ae9500f969",
+    "url": "/monster-rolodex/static/js/runtime-main.e2a8cd7c.js"
+  }
+]);
