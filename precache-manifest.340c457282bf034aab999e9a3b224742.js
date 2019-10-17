51f76cd26d4a94114dc50fdf69e4dfc006fad34b
Joshua Pozos <joshua@pozoss.com>
2019-10-17 12:53:43 -0500

    Updates

---
+++ precache-manifest.340c457282bf034aab999e9a3b224742.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d676deab5db8c13aa772d11c21daf5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "072af3bd4e98c408e3fc",
+    "url": "/monster-rolodex/static/css/main.d6b14491.chunk.css"
+  },
+  {
+    "revision": "e89ff0de3480fcf68967",
+    "url": "/monster-rolodex/static/js/2.9573cf0d.chunk.js"
+  },
+  {
+    "revision": "072af3bd4e98c408e3fc",
+    "url": "/monster-rolodex/static/js/main.8d193428.chunk.js"
+  },
+  {
+    "revision": "c077d3a720ae9500f969",
+    "url": "/monster-rolodex/static/js/runtime-main.e2a8cd7c.js"
+  }
+]);
